a285096a37e9e9c5745df2698ee000957402d0ba
cb-090 <cbabin2005@gmail.com>
2026-03-14 15:21:14 -0700

    Integrated basic HTML + design elements

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,12 @@
-console.log("This is a popup!")
+const tabButton = document.getElementById("tabButton");
+const timerButton = document.getElementById("timerButton");
+const gameButton = document.getElementById("gameButton");
+
+tabButton.addEventListener("change", function() {
+    if (this.checked) {
+        console.log('TAB enabled');
+    }
+    else {
+        console.log('TAB disabled')
+    }
+});
